9fbb928ab3fc3f6233168a815c51cd73d8e5d5a2
Robinnicholas <robinnicholas2000@gmail.com>
2021-10-22 23:23:16 +0530

    completed - need to update changes

--- js/app.js
+++ js/app.js
@@ -3,3 +3,14 @@ function navigation(){
     return nav.classList.toggle("show-on-mobile");
 }
 
+function video(){
+    let videoSection = document.querySelector(".video");
+    let videoTag = document.querySelector("video");
+    if(videoTag.paused){
+        videoTag.play()
+        videoSection.classList.replace("paused", "playing"); 
+    } else{
+        videoTag.pause();
+        videoSection.classList.replace("playing", "paused"); 
+    }
+}

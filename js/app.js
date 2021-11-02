83a21392723f05ff00a95bb13fad6e58ff8d7893
Robinnicholas <robinnicholas2000@gmail.com>
2021-11-02 17:10:33 +0530

    js restructured for video  and some  funtionality changes

--- js/app.js
+++ js/app.js
@@ -6,8 +6,13 @@ function navigation(){
 function video(){
     let videoSection = document.querySelector(".video");
     let videoTag = document.querySelector("video");
-    console.log(videoTag.played.length);    
-    videoSection.classList.replace("ready", "playing"); 
+    
+    // play video
+    videoTag.addEventListener('click', () => {
+        videoSection.classList.replace("ready", "playing"); 
+        videoTag.onended = function(){
+            videoSection.classList.replace("playing", "ready"); 
+        }
         if(videoTag.paused){
             videoTag.play();
             videoSection.classList.replace("paused", "playing"); 
@@ -15,6 +20,29 @@ function video(){
             videoTag.pause();
             videoSection.classList.replace("playing", "paused"); 
         }
+    })
+
+    // change video
+    let users = document.querySelectorAll(".user-details li");
+    users.forEach((eachUser, i) =>{
+        eachUser.addEventListener("click",(e)=>{
+            let videoPoster = eachUser.getAttribute('data-poster');
+            let videoSrc = eachUser.getAttribute('data-src');
+            videoTag.setAttribute("poster", videoPoster);
+            videoTag.setAttribute("src", videoSrc);  
+            videoSection.classList.replace("playing" , "ready");
+            videoSection.classList.replace("paused" , "ready");
+            eachUser.classList.add("active")
+            let allElements = [...users];
+            let otherElements =  allElements.filter((eachElement) => {
+                return eachElement !== eachUser;
+            })
+
+            otherElements.map((eachElement)=>{
+                return eachElement.classList.remove("active");
+            })
+        })
+    })
 }
 
 function task(){
@@ -26,53 +54,7 @@ function task(){
     });
 }
 
-function changeVideo(){
-    let users = document.querySelectorAll(".user-details li");
-    for(let i = 0; i< users.length; i++){
-        users[i].addEventListener("click",(e)=>{        
-            users[i].classList.add("active")
-            let allElements = [...users];
-            let otherElements =  allElements.filter((eachElement) => {
-                return eachElement !== users[i];
-            })
-
-            otherElements.map((eachElement)=>{
-                return eachElement.classList.remove("active");
-            })
-
-            let videoSection = document.querySelector(".video");
-            let videoSrc = document.querySelector("video");
-            switch(i){
-                case 0 : 
-                    videoSrc.setAttribute("poster", "./images/video.jpg");
-                    videoSrc.setAttribute("src", "./images/infinity-war.mp4");
-                    videoSection.classList.replace("playing" , "ready");
-                    videoSection.classList.replace("paused" , "ready");
-                    break;
-                case 1 : 
-                    videoSrc.setAttribute("poster", "./images/bird.jpg");
-                    videoSrc.setAttribute("src", "./images/music.mp4");
-                    videoSection.classList.replace("playing" , "ready");
-                    videoSection.classList.replace("paused" , "ready");
-                    break;
-                case 2 : 
-                    videoSrc.setAttribute("poster", "./images/image-2.jpg");
-                    videoSrc.setAttribute("src", "./images/fireworks.mp4");
-                    videoSection.classList.replace("playing" , "ready");
-                    videoSection.classList.replace("paused" , "ready");
-                    break;
-                case 3 :
-                    videoSrc.setAttribute("poster", "./images/game.jpg");
-                    videoSrc.setAttribute("src", "./images/spiderman.mp4");
-                    videoSection.classList.replace("playing" , "ready");
-                    videoSection.classList.replace("paused" , "ready");
-                    break;
-            }
-        })
-    }
-}
-
 window.addEventListener('load', () => {
     task();
-    changeVideo();
+    video();
 })
